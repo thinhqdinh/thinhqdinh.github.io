8b3c93e9e121094e737936266faed32836a80084
triettranmd <triettran@metro.digital>
2023-09-18 20:46:16 +0700

    enable video toggle

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,11 +3,18 @@ $(document).ready(function() {
   $('a.abstract').click(function() {
     $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
-  });
-  $('a.bibtex').click(function() {
+    $(this).parent().parent().find(".video.hidden.open").toggleClass('open');
+});
+$('a.bibtex').click(function() {
     $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
-  });
+    $(this).parent().parent().find(".video.hidden.open").toggleClass('open');
+});
+$('a.video').click(function() {
+    $(this).parent().parent().find(".video.hidden").toggleClass('open');
+    $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+});
   $('a').removeClass('waves-effect waves-light');
 
   // bootstrap-toc
